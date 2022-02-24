6ef0f72b76d98791ea9544f713a30b99d5403d9e
luuhoaiphong <luuhoaiphong147@gmail.com>
2022-02-24 15:26:54 +0700

    Done category and author function

---
+++ AssignmentFPTBook/Controllers/AuthorsController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AssignmentFPTBook.Models;
+
+namespace AssignmentFPTBook.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        private FPTBookStoreDbContext db = new FPTBookStoreDbContext();
+
+        // GET: Authors
+        public ActionResult Index()
+        {
+            if (Session["UserName"] == Session["UserName"] && Session["Admin"] != null)
+            {
+                return View(db.Authors.ToList());
+            }
+            return View("Error");
+
+        }
+
+        // GET: Authors/Details/5
+        public ActionResult Details(string id)
+        {
+            if (Session["UserName"] == Session["UserName"] && Session["Admin"] != null)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                Author author = db.Authors.Find(id);
+                if (author == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(author);
+            }
+            return View("Error");
+
+
+        }
+
+        // GET: Authors/Create
+        public ActionResult Create()
+        {
+            if (Session["UserName"] == Session["UserName"] && Session["Admin"] != null)
+            {
+                return View();
+            }
+            return View("Error");
+        }
+
+        // POST: Authors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AuthorID,AuthorName,Description")] Author author)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Authors.Add(author);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(author);
+        }
+
+        // GET: Authors/Edit/5
+        public ActionResult Edit(string id)
+        {
+
+            if (Session["UserName"] == Session["UserName"] && Session["Admin"] != null)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                Author author = db.Authors.Find(id);
+                if (author == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(author);
+            }
+            return View("Error");
+
+        }
+
+        // POST: Authors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AuthorID,AuthorName,Description")] Author author)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(author).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(author);
+        }
+
+        // GET: Authors/Delete/5
+        public ActionResult Delete(string id)
+        {
+
+            if (Session["UserName"] == Session["UserName"] && Session["Admin"] != null)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                Author author = db.Authors.Find(id);
+                if (author == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(author);
+            }
+            return View("Error");
+
+        }
+
+        // POST: Authors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Author author = db.Authors.Find(id);
+            db.Authors.Remove(author);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- AssignmentFPTBook/Models/Author.cs
+++ AssignmentFPTBook/Models/Author.cs
@@ -14,14 +14,18 @@ namespace AssignmentFPTBook.Models
             Books = new HashSet<Book>();
         }
 
+        [Required]
         [StringLength(10)]
+        [Display(Name = "Author ID")]
         public string AuthorID { get; set; }
 
         [Required]
         [StringLength(50)]
+        [Display(Name = "Author name")]
         public string AuthorName { get; set; }
 
         [StringLength(100)]
+        [Display(Name = "Description")]
         public string Description { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
