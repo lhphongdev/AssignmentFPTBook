988363ef0f546a41284c43b1a31b58f71cf821b1
luuhoaiphong <luuhoaiphong147@gmail.com>
2022-02-28 12:30:28 +0700

    Cart

---
+++ AssignmentFPTBook/Controllers/ShoppingCartController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AssignmentFPTBook.Models;
+
+namespace AssignmentFPTBook.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        public FPTBookStoreDbContext _db = new FPTBookStoreDbContext();
+
+        public Cart GetCart()
+        {
+            Cart cart = Session["Cart"] as Cart;
+            if (cart == null || Session["Cart"] == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+
+        }
+
+        public ActionResult AddtoCart(string id)
+        {
+            var pro = _db.Books.SingleOrDefault(s => s.BookID == id);
+            if (pro != null)
+            {
+                GetCart().Add(pro);
+            }
+            return RedirectToAction("ShowToCart", "ShoppingCart");
+        }
+
+        public ActionResult UpdateQuantity(FormCollection form)
+        {
+            Cart cart = Session["Cart"] as Cart;
+            string id_pro = form["ID_Product"];
+            int quantity = int.Parse(form["Quantity"]);
+            cart.Update_Quantity_Shopping(id_pro, quantity);
+            return RedirectToAction("ShowToCart", "ShoppingCart");
+        }
+
+        public ActionResult ShowToCart()
+        {
+            if (Session["Cart"] == null)
+                return RedirectToAction("ShowToCart", "ShoppingCart");
+            Cart cart = Session["Cart"] as Cart;
+            return View(cart);
+
+        }
+
+    }
+}
---
+++ AssignmentFPTBook/Models/Cart.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AssignmentFPTBook.Models;
+
+namespace AssignmentFPTBook.Models
+{
+    public class CartItem
+    {
+        public Book _shopping_product { get; set; }
+        public int _shopping_quantity { get; set; }
+    }
+
+    public class Cart
+    {
+        List<CartItem> items = new List<CartItem>();
+        public IEnumerable<CartItem> Items
+        {
+            get { return items; }
+        }
+
+        public void Add(Book _pro, int _quantity = 1)
+        {
+            var item = items.FirstOrDefault(s => s._shopping_product.BookID == _pro.BookID);
+            if (item == null)
+            {
+                items.Add(new CartItem
+                {
+                    _shopping_product = _pro,
+                    _shopping_quantity = _quantity
+                });
+            }
+            else
+            {
+                item._shopping_quantity += _quantity;
+            }
+        }
+
+        public void Update_Quantity_Shopping(string id, int _quantity)
+        {
+            var item = items.Find(s => s._shopping_product.BookID == id);
+            if (item != null)
+            {
+                item._shopping_quantity = _quantity;
+            }
+        }
+    }
+}
